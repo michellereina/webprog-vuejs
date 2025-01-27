c40088c9b22400e56507f975122bd86e461a7a94
michellereina <mbpineda@student.apc.edu.ph>
2025-01-27 12:13:59 +0800

    .

--- vue-project/src/components/PersonalProfile.vue
+++ vue-project/src/components/PersonalProfile.vue
@@ -12,12 +12,6 @@
         <div class="item3">
             <div class="flex-container">
                 <div>
-                    <img class="profpic" src="images/gradpic.JPG" alt="Graduation Picture" style="  width: 1200px; 
-                    max-width: 100%; 
-                    height: auto;
-                    box-shadow: 9px 0px 8px 0px rgba(0, 0, 0, 0.43); ">
-                </div>
-                <div>
                     <p>Hello! Welcome to my personal Website! My name is Ray Mart Villacorte but you can call me RM, from Taguig City. A second year of Asia Pacific College.</p>
                 </div>
             </div>
