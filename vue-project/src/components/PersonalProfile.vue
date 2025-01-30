35699b0989be5ea9a0b0b357b06e3104d4fcc4d5
michellereina <mbpineda@student.apc.edu.ph>
2025-01-30 10:21:41 +0800

    .

--- vue-project/src/components/PersonalProfile.vue
+++ vue-project/src/components/PersonalProfile.vue
@@ -49,4 +49,85 @@
 </template>
 
 <script></script>
-<style></style>
+<style> #app {
+    display: inline-block;
+    padding: 10px;
+    font-size: x-large;
+    background-color: lightgreen;
+  }
+
+  body{
+    padding: 0px;
+    margin: 0px;
+  }
+  
+  .container {
+    display: flex;
+    flex-direction: column;
+    background-color: #DED7EC;
+    height: 100vh;
+  }
+  
+  header, footer {
+    background-color: purple;
+    padding: 20px;
+    color: #DED7EC;
+  }
+  
+  main {
+    display: flex;
+    flex-direction: row;
+  }
+  
+  .main-content {
+    display: flex;
+    flex-wrap: wrap;
+    flex: 1;
+    background-color: #DED7EC;
+    padding: 20px;
+    justify-content: center;
+  }
+  
+
+  .main-content > .grid {
+    background-color: rgba(194, 124, 238, 0.453);
+    border-radius: 20px;
+    width: 800px;
+    height: auto;
+    margin: 10px;
+    padding: 20px;
+    text-align: center;
+    line-height: 50px;
+    color: #3d728d;
+  }
+  .images{
+    width: 1650px;
+    background-color: #2c2c2c;
+    border-radius: 20px;
+    justify-content: center;
+    text-align: center;
+    padding: 25px;
+    color: #4990B3;
+  }
+  img{
+    width: 25%;
+    display: inline-block;
+  }
+  table {
+    border-collapse: collapse;
+    width: 100%;
+  }
+  th, td{
+    padding: 10px;
+    text-align: center;
+    border-bottom: 1px solid #ddd;
+  }
+  .cursor-sparkle {
+    position: absolute;
+    width: 10px;
+    height: 10px;
+    background: #ede4ff;
+    border-radius: 50%;
+    box-shadow: 0 0 10px white;
+    pointer-events: none;
+  }</style>
