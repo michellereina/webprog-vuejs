d23b6d57ed1c70404f440c29cc1923d2010780eb
michellereina <mbpineda@student.apc.edu.ph>
2025-02-04 22:54:22 +0800

    added comment

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,30 +1,14 @@
- <template>
-  <h1>Food</h1>
-    <ul>
-      <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
-    </ul>
-    
-  <food-item/><br>
-  <food-item2/><br>
-  <food-item/><br>
-  <personal-profile/><br>
+<template>
+  <country/>
+  <food-item/>
+  <food-item/>
+  <food-item/>
+  <br>
+  <comment-form/>
+  <comment/>
+  <personal-profile/>
+   
+
 </template>
 
-<script setup>
-  import { ref, onMounted } from 'vue'
-  import { supabase } from './lib/supabaseClient'
-
-  const countries = ref([])
-
-  async function getCountries() {
-    const { data } = await supabase.from('countries').select()
-    countries.value = data
-  }
-
-  onMounted(() => {
-    getCountries()
-  })
-  </script>
-
-<style>
-</style> 
+<script></script>
